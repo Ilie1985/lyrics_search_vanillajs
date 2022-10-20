3dfcbbc234c35e29073ff1627a498e823ad3d35d
Ilie1985 <ilie.marian32@yahoo.com>
2022-10-20 18:15:17 +0100

    get lyrics for song

--- script.js
+++ script.js
@@ -13,21 +13,19 @@ const searchSongs = async (term) => {
   showData(data);
 };
 
-
 //show data and artist in DOM
-
 
 function showData(data) {
   result.innerHTML = `
     <ul class="songs">
       ${data.data
         .map(
-          song => `<li>
+          (song) => `<li>
       <span><strong>${song.artist.name}</strong> - ${song.title}</span>
       <button class="btn" data-artist="${song.artist.name}" data-songtitle="${song.title}">Get Lyrics</button>
     </li>`
         )
-        .join('')}
+        .join("")}
     </ul>
   `;
 
@@ -36,32 +34,39 @@ function showData(data) {
       ${
         data.prev
           ? `<button class="btn" onclick="getMoreSongs('${data.prev}')">Prev</button>`
-          : ''
+          : ""
       }
       ${
         data.next
           ? `<button class="btn" onclick="getMoreSongs('${data.next}')">Next</button>`
-          : ''
+          : ""
       }
     `;
   } else {
-    more.innerHTML = '';
+    more.innerHTML = "";
   }
 }
 
-
 // Get prev and next songs
-const getMoreSongs=async(url)=> {
+const getMoreSongs = async (url) => {
   const res = await fetch(`https://cors-anywhere.herokuapp.com/${url}`);
   const data = await res.json();
 
   showData(data);
-}
+};
 
+// Get lyrics for song
+const getLyrics = async (artist, songTitle) => {
+  const res = await fetch(`${apiURL}/v1/${artist}/${songTitle}`);
+  const data = await res.json();
 
+  const lyrics = data.lyrics.replace(/(\r\n|\r|\n)/g, "<br>");
 
+  result.innerHTML = `<h2><strong>${artist}</strong> - ${songTitle}</h2>
+  <span>${lyrics}</span>`;
 
-
+  more.innerHTML = "";
+};
 
 //event listeners
 
